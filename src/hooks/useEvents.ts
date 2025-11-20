be527d7b540ede0c9cbe201d1db5a67fb4e51148
Marc Levy <marc.levy.to@me.com>
2025-11-19 23:46:40 -0500

    fix events cache key

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 export const useEvents = (latitude: string, longitude: string, game: string) => {
   return useQuery({
-    queryKey: ['events', latitude, longitude],
+    queryKey: ['events', game, latitude, longitude],
     queryFn: async () => {
       const apiUrl = (latitude && longitude) ? `/api/events?game=${game}&latitude=${latitude}&longitude=${longitude}` : `/api/events?game=${game}`;
       const response = await fetch(apiUrl);
